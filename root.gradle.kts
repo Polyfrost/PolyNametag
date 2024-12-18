9fd105d7e6a4782a01ae244d8a13ef7a7d08d7d4
Deftu <me@deftu.dev>
2024-12-18 03:22:18 +0200

    Migrate build files to DGT

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-    kotlin("jvm") version "1.9.10" apply false
-    id("org.polyfrost.multi-version.root")
-    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("dev.deftu.gradle.multiversion-root")
 }
 
 preprocess {
 //    "1.12.2-forge"(11202, "srg") {
-    "1.8.9-forge"(10809, "srg")
+//        "1.12.2-fabric"(11202, "yarn") {
+            "1.8.9-fabric"(10809, "yarn") {
+                "1.8.9-forge"(10809, "srg")
+            }
+//        }
 //    }
 }
